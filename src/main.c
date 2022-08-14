8e9fe79321c7963fd6b9d969d2be092cdf9bb6bd
Nishant <qweenjon@gmail.com>
2022-08-13 19:02:04 -0700

    added lexing to main function

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
     /* create a new .html file with the same name as the .buml one */
-    //FILE* input_file = argv[1];
+    FILE* input_file = argv[1];
     printf("%s\n", argv[1]);
     char* new_file_name; /* to create new HTML file */
     new_file_name = strdup(argv[1]); /* copy the name of the .buml file */
@@ -20,6 +20,10 @@ int main(int argc, char** argv) {
     new_file_name[len + 2] = 'm';
     new_file_name[len + 3] = 'l';
     printf("%s\n", new_file_name);
+
+    /* lex and parse the new HTML file and push html code into .html file */
+    FILE* html_file = fopen(new_file_name, "+w");
+    lex(input_file, html_file);
 
     free(new_file_name);
 }
